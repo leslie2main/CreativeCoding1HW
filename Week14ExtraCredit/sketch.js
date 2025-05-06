26fbd92ed3c9c7eb0f13b0f76f9f47166221a087
leslie2main <leslie.main@umontana.edu>
2025-05-05 23:30:22 -0600

    Update sketch.js

---
+++ Week14ExtraCredit/sketch.js
@@ -0,0 +1,151 @@
+let obstacles = [];
+let player;
+let exitArea;
+let gameWon = false;
+
+let shapes = []; // Array for shape objects
+
+function setup() {
+  createCanvas(800, 600);
+
+  // Create player
+  player = createVector(50, height / 2);
+
+  // Create exit area
+  exitArea = {
+    x: width - 100,
+    y: height - 100,
+    w: 80,
+    h: 80
+  };
+
+  // Create initial obstacles
+  for (let i = 0; i < 5; i++) {
+    obstacles.push(createRandomObstacle(true)); // moving
+  }
+
+  // Create shape objects and store in array
+  shapes.push(new SimpleShape(100, 100, 40, 40, color(255, 0, 0)));
+  shapes.push(new SimpleShape(300, 200, 60, 30, color(0, 255, 0)));
+  shapes.push(new SimpleShape(500, 350, 50, 50, color(0, 0, 255)));
+}
+
+function draw() {
+  background(30);
+
+  // Exit area
+  fill(0, 200, 0);
+  rect(exitArea.x, exitArea.y, exitArea.w, exitArea.h);
+  textSize(14);
+  fill(255);
+  text("EXIT", exitArea.x + 20, exitArea.y + 45);
+
+  // Draw and update obstacles
+  for (let obs of obstacles) {
+    fill(obs.color);
+    rect(obs.x, obs.y, obs.w, obs.h);
+
+    if (obs.moving) {
+      obs.x += obs.vx;
+      obs.y += obs.vy;
+
+      // Wrap around
+      if (obs.x > width) obs.x = -obs.w;
+      if (obs.x + obs.w < 0) obs.x = width;
+      if (obs.y > height) obs.y = -obs.h;
+      if (obs.y + obs.h < 0) obs.y = height;
+    }
+  }
+
+  // Draw shape objects from the class
+  for (let shape of shapes) {
+    shape.display();
+  }
+
+  // Draw player
+  fill(0, 150, 255);
+  ellipse(player.x, player.y, 20, 20);
+
+  if (gameWon) {
+    textSize(48);
+    fill(255);
+    textAlign(CENTER, CENTER);
+    text("You Win!", width / 2, height / 2);
+    noLoop();
+  }
+
+  checkWin();
+}
+
+function keyPressed() {
+  const speed = 5;
+  if (key === 'w' || key === 'W') player.y -= speed;
+  if (key === 's' || key === 'S') player.y += speed;
+  if (key === 'a' || key === 'A') player.x -= speed;
+  if (key === 'd' || key === 'D') player.x += speed;
+}
+
+function mousePressed() {
+  // Add a new moving obstacle at mouse location
+  obstacles.push({
+    x: mouseX,
+    y: mouseY,
+    w: random(20, 60),
+    h: random(20, 60),
+    color: color(random(255), random(255), random(255)),
+    vx: random(-2, 2),
+    vy: random(-2, 2),
+    moving: true
+  });
+}
+
+function createRandomObstacle(moving) {
+  return {
+    x: random(width),
+    y: random(height),
+    w: random(30, 100),
+    h: random(30, 100),
+    color: color(random(255), random(255), random(255)),
+    vx: moving ? random(-2, 2) : 0,
+    vy: moving ? random(-2, 2) : 0,
+    moving: moving
+  };
+}
+
+function checkWin() {
+  if (
+    player.x > exitArea.x &&
+    player.x < exitArea.x + exitArea.w &&
+    player.y > exitArea.y &&
+    player.y < exitArea.y + exitArea.h
+  ) {
+    gameWon = true;
+  }
+}
+
+// Simple shape class
+class SimpleShape {
+  constructor(x, y, w, h, col) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.col = col;
+  }
+
+  display() {
+    // Highlight if mouse over
+    if (
+      mouseX > this.x && mouseX < this.x + this.w &&
+      mouseY > this.y && mouseY < this.y + this.h
+    ) {
+      stroke(255);
+      strokeWeight(2);
+    } else {
+      noStroke();
+    }
+
+    fill(this.col);
+    rect(this.x, this.y, this.w, this.h);
+  }
+}
